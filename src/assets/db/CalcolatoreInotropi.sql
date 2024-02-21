f7af8e95c2aaf84d827dbe6311c6f021fc89362f
GiuseppeSoriano <giuseppesoriano02@gmail.com>
2024-02-21 13:44:08 +0100

    UpdateDatabase

--- src/assets/db/CalcolatoreInotropi.sql
+++ src/assets/db/CalcolatoreInotropi.sql
@@ -26,11 +26,11 @@ CREATE TABLE `otherfarmaco` (
 INSERT INTO `otherfarmaco` VALUES
 ('B001', 'Remifentanil flacone 1 mg', 1, null, '0.025-2 mcg/kg/min'),
 ('B002', 'Remifentanil flacone 5 mg', 5, null, '0.025-2 mcg/kg/min'),
-('B003', 'Midazolam fiale 5 mg / ml', 5, 1, '0.03-0.2 mcg/kg/min'),
-('B004', 'Midazolam fiale 15 mg /3 ml', 15, 3, '0.03-0.2 mcg/kg/min'),
-('B005', 'Propofol 1% 200 mg / 20 ml', 200, 20, '0.3-12 mcg/kg/min'),
-('B006', 'Dexmedetomidina 200 mcg / 2 ml', 0.2, 2, '0.2-1.4 mcg/kg/min'),
-('B007', 'Dexmedetomidina 400 mcg / 4 ml', 0.4, 4, '0.2-1.4 mcg/kg/min'),
+('B003', 'Midazolam fiale 5 mg / ml', 5, 1, '0.03-0.2 mg/kg/h'),
+('B004', 'Midazolam fiale 15 mg /3 ml', 15, 3, '0.03-0.2 mg/kg/h'),
+('B005', 'Propofol 1% 200 mg / 20 ml', 200, 20, '0.3-12 mg/kg/h'),
+('B006', 'Dexmedetomidina 200 mcg / 2 ml', 0.2, 2, '0.2-1.4 mcg/kg/h'),
+('B007', 'Dexmedetomidina 400 mcg / 4 ml', 0.4, 4, '0.2-1.4 mcg/kg/h'),
 ('B008', 'Levosimendan 12,5 mg / 5 ml', 12.5, 5, '0.05-0.2 mcg/kg/min'),
 ('B009', 'Levosimendan 25 mg / 10 ml', 25, 10, '0.05-0.2 mcg/kg/min');
 
